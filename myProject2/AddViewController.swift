a81f3361720aa9cf5c7dce3caef3e11628b2498b
haya <haya.Adblrhman.med.s@tuwaiq.edu.sa>
2021-11-03 23:18:39 +0300

    Take photo From Library

--- myProject2/AddViewController.swift
+++ myProject2/AddViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AddViewController: UIViewController {
+class AddViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     var FlowerModel : Store!
     var selectedFlowerIndex : Int!
 
@@ -43,4 +43,44 @@ class AddViewController: UIViewController {
         navigationController?.popViewController(animated: true)
         
     }
+    @IBAction func Add(_ sender: Any) {
+        showpohto()
+  }
+    func showpohto(){
+        let alert = UIAlertController(title: "Take photo From ", message: " Thanks For adding ", preferredStyle: .actionSheet)
+        
+        
+        
+        alert.addAction(UIAlertAction(title: "photo library", style: .default , handler: {action in
+            self.getPhoto(type: .photoLibrary)
+            
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+   }
+    
+    func getPhoto(type:UIImagePickerController.SourceType){
+        let picker = UIImagePickerController()
+        picker.sourceType = type
+        picker.allowsEditing = false
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        dismiss(animated: true, completion: nil)
+       
+        guard let image = info[.originalImage] as? UIImage else{
+            print ("image not found")
+            return
+        }
+        Addimag.image = image
+        
+        
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
+
